746c319db5e29212e0761cd18a07e4ddef14c5f1
Rodrigo <PachecoRodrigo97@hotmail.com>
2021-02-04 23:14:40 -0300

    TodoApp

---
+++ README.md
@@ -0,0 +1,16 @@
+# Webpack Starter
+
+Este es el proyecto inicial para crear aplicaciones usando webpack.
+
+### Notas:
+Recuerden reconstruir los módulos de Node
+
+```
+npm install
+```
+
+Y para construir el build:
+
+```
+npm run build
+```
---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,52 @@
+export class TodoList {
+
+    constructor() {
+        this.cargarLocalStorage();
+    }
+
+    nuevoTodo( todo ) {
+        this.todos.push( todo );
+        this.guardarLocalStorage();
+    }
+
+    eliminarTodo( id ) {
+
+        this.todos = this.todos.filter( todo => todo.id != id );
+        this.guardarLocalStorage();
+
+    }
+
+    marcarCompletado( id ) {
+
+        for ( const todo of this.todos ) {
+
+            if ( todo.id == id) {
+
+                todo.completado = !todo.completado;
+                this.guardarLocalStorage();
+                break;
+            }
+        }
+
+    }
+
+    eliminarCompletados() {
+
+        this.todos = this.todos.filter( todo => !todo.completado );
+        this.guardarLocalStorage();
+
+    }
+
+    guardarLocalStorage() {
+   
+        localStorage.setItem( 'todo', JSON.stringify( this.todos ) );
+
+    }
+
+    cargarLocalStorage() {
+
+        this.todos = JSON.parse(localStorage.getItem('todo')) || [];
+
+    }
+
+}
